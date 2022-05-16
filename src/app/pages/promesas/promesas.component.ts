659f7cfee8ca38286bc12ab7283c806d73229cbe
Camilo López <camiloj.lopez79@gmail.com>
2022-05-16 16:29:49 -0400

    Funciones que retornan promesas

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -3,24 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-promesas',
   templateUrl: './promesas.component.html',
-  styleUrls: ['./promesas.component.css']
+  styleUrls: []
 })
 export class PromesasComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-    const promesa = new Promise( ( resolve, reject ) => {
-      if ( false ) {
-        resolve( 'Hola Mundo' );
-      } else {
-        reject( 'Algo salió mal' );
-      }
+    this.getUsers().then( users => {
+      console.log( users );
     });
-    promesa.then( ( msg ) => {
-      console.log( msg );
-    }).catch( err => console.log( 'Error en mi pormesa:', err ) );
+  }
 
-    console.log( 'Fin del Init' );
+  getUsers() {
+    return new Promise( resolve => {
+      fetch( 'https://reqres.in/api/users' )
+        .then( resp => resp.json() )
+        .then( body => resolve( body.data ) );
+    });
   }
 }
