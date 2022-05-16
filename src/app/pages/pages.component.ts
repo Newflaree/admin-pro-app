7d5d0bcac4f79336b11b50466be249ed17beb433
Camilo López <camiloj.lopez79@gmail.com>
2022-05-16 09:57:32 -0400

    Cambiar el CSS principal de forma dinámica

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -7,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class PagesComponent implements OnInit {
+  public linkTheme = document.querySelector( '#theme' );
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const currentTheme = localStorage.getItem('theme') || `./assets/css/colors/default.css`;
 
+    this.linkTheme?.setAttribute( 'href', currentTheme );
+  }
 }
