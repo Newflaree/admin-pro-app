48a8ace866c6b0a60558ad77c877e4e1d652bae4
Camilo López <camiloj.lopez79@gmail.com>
2022-05-25 16:06:58 -0400

    Validaciones del formulario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -9,18 +9,40 @@ import { FormBuilder, Validators } from '@angular/forms';
   ]
 })
 export class RegisterComponent {
+  public fromSubmitted = false;
+
   public registerForm = this.fb.group({
-    name: [ 'TestTest', [ Validators.required, Validators.minLength(3) ] ],
-    email: [ 'test11@email.com', Validators.required ],
-    password: [ '123456', Validators.required ],
-    password2: [ '123456', Validators.required ],
+    name: [ '', [ Validators.required, Validators.minLength(3) ] ],
+    email: [ '', [ Validators.required, Validators.email ] ],
+    password: [ '', Validators.required ],
+    password2: [ '', Validators.required ],
     terms: [ false, Validators.required ]
   });
 
   constructor( private fb: FormBuilder ) { }
 
   createUser() {
+    this.fromSubmitted = true;
     console.log( this.registerForm.value );
+    if ( this.registerForm.valid ) {
+      console.log( 'Posting form' );
+
+    } else {
+      console.log( 'Invalid form' );
+    }
   }
 
+  invalidField( field: string ): boolean {
+    if ( this.registerForm.get( field )?.invalid && this.fromSubmitted ) {
+      return true;
+
+    } else {
+      return false;
+
+    }
+  }
+
+  acceptTemrs() {
+    return !this.registerForm.get( 'terms' )?.value && this.fromSubmitted;
+  }
 }
