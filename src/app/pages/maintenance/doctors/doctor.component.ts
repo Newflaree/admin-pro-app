8d9ed78d06f011f10bdc814da71cb0d1f6ef7fa9
Camilo López <camiloj.lopez79@gmail.com>
2022-06-08 00:36:04 -0400

    Preparar el formulario de creación de doctors

--- src/app/pages/maintenance/doctors/doctor.component.ts
+++ src/app/pages/maintenance/doctors/doctor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Hospital} from 'src/app/models';
+import {HospitalService} from 'src/app/services';
 
 @Component({
   selector: 'app-doctor',
@@ -7,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DoctorComponent implements OnInit {
+  public doctorForm: FormGroup;
+  public hospitals: Hospital[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private hospitalService: HospitalService
+  ) { 
+    this.doctorForm = this.fb.group({
+      name: [ 'GG', Validators.required ],
+      hospital: [ '', Validators.required ]
+    })
   }
 
+  ngOnInit(): void {
+    this.loadHospitals();
+  }
+
+  loadHospitals() {
+    this.hospitalService.loadHospitals()
+    .subscribe( ({ hospitals }) => this.hospitals = hospitals || [] )
+  }
+
+  saveDoctor() {
+    console.log( this.doctorForm.value );
+  }
 }
